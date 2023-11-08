07d3d4dee1502ca58d6a3c37efefb738e3094a0d
Truong <dyanmictruong@gmail.com>
2023-11-08 12:03:09 +0700

    css register components

--- src/Components/Register/index.jsx
+++ src/Components/Register/index.jsx
@@ -8,7 +8,8 @@ function Register(props) {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
-  const [rewritePasswords, setRewritePasswords] = useState("");
+  const [rewritePassword, setRewritePassword] = useState("");
+  const [telephone, setTelephone] = useState("");
 
   const writeAccount = (event) => {
     setAccount(event.target.value);
@@ -26,8 +27,12 @@ function Register(props) {
     setBirthday(event.target.value);
   };
 
-  const rewrite_passwords = (event) => {
-    setRewritePasswords(event.target.value);
+  const rewrite_password = (event) => {
+    setRewritePassword(event.target.value);
+  };
+
+  const writeTelephone = (event) => {
+    setTelephone(event.target.value);
   };
 
   const registeredForm = () => {
@@ -37,10 +42,14 @@ function Register(props) {
       email === "user01@gmail.com"
     ) {
       alert("Account, password and email is available");
-    } else if (email.includes("@", 1)) {
+    } else if (!email.includes("@", 1)) {
       alert("Check your email");
-    } else if (password !== rewritePasswords.includes(password)) {
+    } else if (password != rewritePassword) {
       alert("Check your password");
+    } else if (!telephone && telephone.length < 10) {
+      alert("Fill in your phone number and 10 characters");
+    } else if (password.length < 10 && account.length < 10) {
+      alert("You need fill in password and account 10 characters");
     } else {
       props.login("login");
       alert("Register success");
@@ -49,7 +58,7 @@ function Register(props) {
     nextRegisterForm.push({
       account: account,
       password: password,
-      passwordAgain: rewritePasswords,
+      passwordAgain: rewritePassword,
       email: email,
       birthday: birthday,
     });
@@ -58,7 +67,8 @@ function Register(props) {
     setPassword("");
     setEmail("");
     setBirthday("");
-    setRewritePasswords("");
+    setRewritePassword("");
+    setTelephone("");
   };
 
   return (
@@ -67,34 +77,65 @@ function Register(props) {
         <h1>ĐĂNG KÝ</h1>
         <div className="mini-container">
           <span>Tên tài khoản:</span>{" "}
-          <input type="text" onChange={writeAccount} value={account} />
+          <input
+            type="text"
+            onChange={writeAccount}
+            value={account}
+            className="btn-register"
+          />
         </div>
         <div className="mini-container">
           <span>Email:</span>{" "}
-          <input type="email" onChange={writeEmail} value={email} />
+          <input
+            type="email"
+            onChange={writeEmail}
+            value={email}
+            className="btn-register"
+          />
         </div>
         <div className="mini-container">
           <span>Mật khẩu:</span>{" "}
-          <input type="password" onChange={writePassword} value={password} />
+          <input
+            type="password"
+            onChange={writePassword}
+            value={password}
+            className="btn-register"
+          />
         </div>
         <div className="mini-container">
           <span>Nhập lại mật khẩu:</span>{" "}
           <input
             type="password"
-            value={rewritePasswords}
-            onChange={rewrite_passwords}
+            value={rewritePassword}
+            onChange={rewrite_password}
+            className="btn-register"
           />
         </div>
         <div className="mini-container">
           <span>Số điện thoại: </span>
-          <input type="tel" />
+          <input
+            type="tel"
+            value={telephone}
+            onChange={writeTelephone}
+            className="btn-register"
+          />
         </div>
         <div>
           <span>Giới tính: Nam </span>
-          <input type="radio" /> <span>Nữ</span> <input type="radio" />
-          <span>Ngày sinh:</span> <input type="date" onChange={writeBirthday} />
+          <input type="radio" className="btn-register" />
+          <span>Nữ</span>
+          <input type="radio" className="btn-register" />
+          <span>Ngày sinh:</span>{" "}
+          <input
+            type="date"
+            onChange={writeBirthday}
+            value={birthday}
+            className="btn-register"
+          />
         </div>
-        <button onClick={registeredForm}>Đăng ký</button>
+        <button onClick={registeredForm} className="registerBtn">
+          Đăng ký
+        </button>
       </div>
     </div>
   );
